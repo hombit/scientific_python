0fa0976f651e663e0444113ac28bcb2001e6a655
Konstantin Malanchev <hombit@gmail.com>
2017-07-06 14:22:01 +0300

    Add script for virtualenv activation

---
+++ virtualenv_activation.sh
@@ -0,0 +1,20 @@
+# Creates and activates virtualenv for the scientific_python project.
+# Do not call it directly. Instead type
+# $ . ./virtualenv_activation.sh
+# or
+# $ source ./virtualenv_activation.sh
+# You can deactivate virtualenv typing
+# $ deactivate
+
+
+ENV=$HOME/.virtualenvs/scientific_python
+mkdir -vp $ENV
+virtualenv $ENV
+source $ENV/bin/activate
+
+PYTHON_PATH=$(which python)
+if [ "${PYTHON_PATH/$ENV}" = "${PYTHON_PATH}" ]; then
+    echo "Activation failed"
+else
+    python setup.py install
+fi
