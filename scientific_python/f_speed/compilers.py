3c78e3362d48d58ac3b6a182f1b764762d5a251c
Konstantin Malanchev <hombit@gmail.com>
2017-12-08 12:02:14 +0300

    Probably fix import inside timeit

--- scientific_python/f_speed/compilers.py
+++ scientific_python/f_speed/compilers.py
@@ -10,16 +10,17 @@ timeit_kwargs= {
     'stmt' : 'contfrac(a)',
     'number': 10,
     'setup': """import numpy as np
-from contfrac import {f}_contfrac as contfrac
+from {dot}contfrac import {func}_contfrac as contfrac
 a = np.ones({n})""",
 }
 
 n = 1000000
+dot = '.' if __package__ else ''
 f_types = ['pure', 'numba', 'cython']
 time = {}
 for f in f_types:
     kwargs = copy(timeit_kwargs)
-    kwargs['setup'] = kwargs['setup'].format(f=f, n=n)
+    kwargs['setup'] = kwargs['setup'].format(func=f, dot=dot, n=n)
     time[f] = timeit(**kwargs)
 assert time['pure'] > time['numba']
 assert time['pure'] > time['cython']
